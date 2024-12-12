5600016655d7e323fa3b5dd000918d53fd88df55
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-12-12 13:48:22 +0900

    chore: 프로젝트 기본 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+// provider
+import { MswComponent } from '@/components/provider/MswComponent';
+import { TanStackQueryProvider } from '@/components/provider/QueryClient';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const MSWState = process.env.NEXT_PUBLIC_MSW_STATE === 'true';
+  return (
+    <TanStackQueryProvider>
+      {MSWState ? (
+        <MswComponent>
+          <Component {...pageProps} />
+        </MswComponent>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </TanStackQueryProvider>
+  );
 }
